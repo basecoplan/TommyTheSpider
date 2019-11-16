5cbb5112c3a06e105211f9664459b89480f25086
LoserAntbear <tollemet@gmail.com>
2019-11-16 22:30:51 +0300

    TTS-0: Fix crlf. Add phaser game init. Add basic body\html styles.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
+import Phaser from 'phaser';
+
 import './style.css';
-// import Phaser from 'phaser';
+
+import { BASE_GAME_CONFIG } from './config/index';
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+const config: Phaser.Types.Core.GameConfig = {
+  ...BASE_GAME_CONFIG,
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+const GAME = new Phaser.Game(config);
+
+export default GAME;
