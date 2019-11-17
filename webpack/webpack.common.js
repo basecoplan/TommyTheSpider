cd13b621e856d813f74efab457624ebc2ced993c
LoserAntbear <tollemet@gmail.com>
2019-11-17 04:04:51 +0300

    TTS-0: Add sass loader

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,50 +3,55 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
-  entry: './src/index.ts',
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Tommy The Spider',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-              onlyCompileBundledFiles: true,
-              experimentalWatchApi: true,
-            },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml)$/,
-        use: [
-          'file-loader',
-        ],
-      },
+    entry: './src/index.ts',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Tommy The Spider',
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-  },
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                        onlyCompileBundledFiles: true,
+                        experimentalWatchApi: true,
+                    },
+                }, ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|xml)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
 };
