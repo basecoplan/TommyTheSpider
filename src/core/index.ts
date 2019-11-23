5fec9a68ad0cd672b1fee242165f836aaf6ac3f4
LoserAntbear <tollemet@gmail.com>
2019-11-23 21:17:15 +0300

    TTS-0: Rewrite `Actor` to extend `BaseEntity`

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,38 +1,12 @@
 /* eslint-disable max-classes-per-file */
-import { Mesh } from '@babylonjs/core/Meshes/mesh';
 import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder';
 import { Scene } from '@babylonjs/core/scene';
 import { StandardMaterial, Color3 } from '@babylonjs/core';
 
-import { IActor } from '../types';
-import { generateId } from '../utils/id-generator';
-import { SystemsIds } from '../enums/systems-ids';
-import { ComponentsIds } from '../enums/components-ids';
+import { BaseEntity } from './entities/base-enity';
+import MeshComponent from './components/mesh';
 
-class Actor implements IActor {
-  public get id(): string {
-    return this._id;
-  }
-
-  public get mesh(): Mesh {
-    return this._mesh;
-  }
-
-  public get components(): Set<ComponentsIds> {
-    return this._components;
-  }
-
-  protected _id: string;
-  protected _mesh: Mesh;
-  protected _components: Set<ComponentsIds>;
-
-  constructor() {
-    this._id = generateId();
-    this._components = new Set();
-  }
-
-  public initialize(scene: Scene): void {}
-}
+class Actor extends BaseEntity {}
 
 
 export class Enemy extends Actor {
@@ -43,8 +17,9 @@ export class Enemy extends Actor {
 
     const mesh = MeshBuilder.CreateBox('Box', { size: 4 }, scene);
     mesh.material = material;
+    const meshComponent = new MeshComponent(mesh);
 
-    this._mesh = mesh;
+    this._components.add(meshComponent, this);
   }
 }
 
@@ -55,10 +30,9 @@ export class Player extends Actor {
 
     const mesh = MeshBuilder.CreatePolyhedron('Actor', { type: 0, size: 2 }, scene);
     mesh.material = material;
+    const meshComponent = new MeshComponent(mesh);
 
-    this._mesh = mesh;
-    this._position = mesh.position;
-    this._rotation = mesh.rotation;
+    this._components.add(meshComponent, this);
   }
 }
 
