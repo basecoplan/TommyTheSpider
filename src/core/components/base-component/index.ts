664b3f7b4f1be4ae8c3209a3d998a81ea88a543f
LoserAntbear <tollemet@gmail.com>
2019-11-23 22:17:45 +0300

    TTS-0: WIP. Add `BaseComponent` abstract class

---
+++ src/core/components/base-component/index.ts
@@ -0,0 +1,17 @@
+import { IComponent, IEntity } from "../../../types";
+import { ComponentsIds } from "../../../enums/components-ids";
+
+export abstract class BaseComponent<T> implements IComponent<T> { 
+    public get value(): T {
+        return this._value;
+    }
+
+    public abstract get entities(): Set<IEntity>;
+
+    public static entities: Set<IEntity>;
+    public id: ComponentsIds;
+
+    constructor(
+        private _value: T
+    ) {}
+}
