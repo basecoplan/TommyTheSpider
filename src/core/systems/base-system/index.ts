28d988b50c66f601ea1a6a383e47155a7e3a5c98
LoserAntbear <tollemet@gmail.com>
2019-11-23 21:18:33 +0300

    TTS-0: Add `Base System`

--- src/core/systems/base-system/index.ts
+++ src/core/systems/base-system/index.ts
@@ -1,18 +1,20 @@
-import { ISystem } from "../../../types";
-import { SystemsIds } from "../../../enums/systems-ids";
+import { ISystem, IEntity } from '../../../types';
+import { SystemsIds } from '../../../enums/systems-ids';
 
-export class BaseSystem implements ISystem {
+export abstract class BaseSystem implements ISystem {
     public get id(): SystemsIds {
         return this._id;
     }
 
-    public get entities(): Set<string> {
+    public get entities(): Set<IEntity> {
         return this._entities;
     }
 
-    protected _entities: Set<string> = new Set([]);
+    protected _entities: Set<IEntity> = new Set([]);
 
     constructor(
         protected _id: SystemsIds
     ) {}
+
+    protected abstract _processEntity(entityId: IEntity): void;
 }
