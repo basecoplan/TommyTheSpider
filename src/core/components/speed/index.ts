dc9fd92772b25a99a63f6a38b0a2ffb7eb5a84dc
LoserAntbear <tollemet@gmail.com>
2019-11-23 22:22:54 +0300

    TTS-0: Update existing components to extend `BaseComponent`

--- src/core/components/speed/index.ts
+++ src/core/components/speed/index.ts
@@ -1,13 +1,12 @@
-import { Mesh } from "@babylonjs/core";
 import { IEntity } from "../../../types";
 import { ComponentsIds } from "../../../enums/components-ids";
 import { BaseComponent } from "../base-component";
 
 const SET: Set<IEntity> = new Set();
 
-export class MeshComponent extends BaseComponent<Mesh> { 
+export class SpeedComponent extends BaseComponent<number> { 
     public get entities(): Set<IEntity> {
-        return MeshComponent.entities;
+        return SpeedComponent.entities;
     }
 
     public static entities: Set<IEntity> = SET;
--- src/core/systems/follow-main-arrow-pointer-system/index.ts
+++ src/core/systems/follow-main-arrow-pointer-system/index.ts
@@ -3,7 +3,7 @@ import { PointerInfo } from "@babylonjs/core";
 import { SystemsIds } from "../../../enums/systems-ids";
 import { BaseSystem } from "../base-system";
 import { IEntity } from "../../../types";
-import MeshComponent from "../../components/mesh";
+import { MeshComponent } from "../../components/mesh";
 import { ComponentsIds } from "../../../enums/components-ids";
 
 class FollowMainArrowPointerSystem extends BaseSystem {
@@ -17,15 +17,14 @@ class FollowMainArrowPointerSystem extends BaseSystem {
         this._pointerInfo = info;
 
         console.log(this._pointerInfo);
+    }
+
+    protected _initializeEntities(): void {
         this._entities = new Set(MeshComponent.entities);
     }
-
-    public process(): void {
-        this._entities.forEach(this._processEntity);
-    }
-
+    
     protected _processEntity(entity: IEntity): void {
-        const { mesh } = entity.components.get(ComponentsIds.Mesh) as MeshComponent;
+        const { value: mesh } = entity.components.get(ComponentsIds.Mesh) as MeshComponent;
     }
 }
 
